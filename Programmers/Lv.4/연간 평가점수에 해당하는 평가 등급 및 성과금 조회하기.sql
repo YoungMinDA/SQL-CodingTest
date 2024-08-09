27507465846bf13f249034525f09a9a51147bd99
YoungMin98 <109095108+YoungMinDA@users.noreply.github.com>
2024-08-09 13:34:29 +0900

    Add files via upload

---
+++ Programmers/Lv.4/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,28 @@
+-- 코드를 작성해주세요
+
+WITH AVGSCORE AS (
+    SELECT EMP_NO, AVG(SCORE) AS SCORE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+
+SELECT 
+    E.EMP_NO
+    , E.EMP_NAME
+    , CASE 
+        WHEN G.SCORE >= 96 THEN 'S'
+        WHEN G.SCORE >= 90 THEN 'A'
+        WHEN G.SCORE >= 80 THEN 'B'
+        ELSE 'C'
+        END AS GRADE
+    , CASE 
+        WHEN G.SCORE >= 96 THEN E.SAL * 0.2
+        WHEN G.SCORE >= 90 THEN E.SAL * 0.15
+        WHEN G.SCORE >= 80 THEN E.SAL * 0.1
+        ELSE 0
+        END AS BONUS 
+FROM HR_EMPLOYEES E
+JOIN AVGSCORE G
+ON E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO
+ORDER BY EMP_NO ASC;
